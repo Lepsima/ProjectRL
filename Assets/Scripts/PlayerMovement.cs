cb535ce8c83ef1d842415954e866c55a57d15506
Ralatz <76174827+Ralatz@users.noreply.github.com>
2025-09-30 00:56:03 +0200

    Movimiento Basico(A,D)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed;
+    private Rigidbody2D rb2D;
+
+    private float move;
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        move = Input.GetAxis("Horizontal");
+        rb2D.linearVelocity = new Vector2(move * speed, rb2D.linearVelocity.y);
     }
 }
