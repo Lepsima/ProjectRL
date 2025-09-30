478f9fc7b81cb8d4806baed44c10d738ddbb41b9
Ralatz <76174827+Ralatz@users.noreply.github.com>
2025-09-30 18:57:52 +0200

    Enemigo Basico(Perseguir a Jugador y Dejar de seguirlo)

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    //Variables
+    public Transform player;
+    public float detectionradius;
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        // Deteccion de Jugador
+        float distanceToPlayer = Vector2.Distance(transform.position,player.position);
+
+        if (distanceToPlayer < detectionradius)
+        {
+
+            Vector2 direction = (player.position - transform.position).normalized;
+
+            movement = new Vector2(direction.x, 0);
+        }
+        else
+        {
+            movement = Vector2.zero;
+        }
+
+        //Movimiento
+
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+
+
+        // Area de radio
+         void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position,detectionradius);
+        }
+
+    }
+}
